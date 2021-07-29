fac2a78bd31101107a620ae3872f10a651188614
Andeser-rgb <andeser@protonmail.com>
2021-07-29 21:25:34 +0200

    ensure authenticated

--- server.js
+++ server.js
@@ -1,72 +1,85 @@
-'use strict';
-require('dotenv').config();
-const express = require('express');
-const myDB = require('./connection');
-const fccTesting = require('./freeCodeCamp/fcctesting.js');
-const session = require('express-session');
-const passport = require('passport');
-const ObjectID = require('mongodb').ObjectID;
-const LocalStrategy = require('passport-local');
+"use strict";
+require("dotenv").config();
+const express = require("express");
+const myDB = require("./connection");
+const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const session = require("express-session");
+const passport = require("passport");
+const ObjectID = require("mongodb").ObjectID;
+const LocalStrategy = require("passport-local");
 
 const app = express();
 
-
-
 fccTesting(app); //For FCC testing purposes
-app.set('view engine', 'pug');
-app.use('/public', express.static(process.cwd() + '/public'));
+app.set("view engine", "pug");
+app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: true,
-  saveUninitialized: true,
-  cookie: {secure: false}
-}));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-myDB(async client => {
-  const myDataBase = await client.db('database').collection('users');
-  app.route('/').get((req, res) => {
-    res.render(process.cwd() + '/views/pug', {
-        title: "Connected to Database", 
-        message: 'Please login',
-        showLogin: true
-      });
+myDB(async (client) => {
+  const myDataBase = await client.db("database").collection("users");
+  app.route("/").get((req, res) => {
+    res.render(process.cwd() + "/views/pug", {
+      title: "Connected to Database",
+      message: "Please login",
+      showLogin: true,
+    });
   });
   passport.serializeUser((user, done) => {
     done(null, user._id);
   });
   passport.deserializeUser((id, done) => {
-    myDataBase.findOne({_id: new ObjectID(id)}, (err, doc) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
       done(null, doc);
     });
   });
 
-  passport.use(new LocalStrategy(
-    (username, password, done) => {
-      myDataBase.findOne({username: username}, (err, user) => {
-        console.log('User ' + username + ' attempted to login');
-        if(err) return done(err);
-        if(!user) return done(null, false);
-        if(password !== user.password) return done(nulll, false);
+  passport.use(
+    new LocalStrategy((username, password, done) => {
+      myDataBase.findOne({ username: username }, (err, user) => {
+        console.log("User " + username + " attempted to login");
+        if (err) return done(err);
+        if (!user) return done(null, false);
+        if (password !== user.password) return done(null, false);
         return done(null, user);
       });
-    }
-  ));
+    })
+  );
 
-  app.post('/login', passport.authenticate('local', {failureRedirect: '/'}));
+  app.post("/login", passport.authenticate("local", { failureRedirect: "/" }));
 
-}).catch(e => {
-  app.route('/').get((req, res) => {
-    res.render(process.cwd() + '/views/pug', { title: e, message: 'Unable to login' });
+  app.get("/profile", ensureAuthenticated, (req, res) => {
+    res.render(process.cwd() + "/views/pug/profile");
+  });
+}).catch((e) => {
+  app.route("/").get((req, res) => {
+    res.render(process.cwd() + "/views/pug", {
+      title: e,
+      message: "Unable to login",
+    });
   });
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
+  console.log("Listening on port " + PORT);
 });
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/");
+}
