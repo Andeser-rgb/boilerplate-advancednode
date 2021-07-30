08b7fb57cb57f037974ba0dcdd7cd93b4ba84742
Andeser-rgb <andeser@protonmail.com>
2021-07-30 12:35:50 +0200

    Authentication with Socket.IO

--- server.js
+++ server.js
@@ -16,6 +16,15 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
+const passportSocketIo = require('passport.socketio');
+const MongoStore = require('connect-mongo')(session);
+const cookieParser = require('cookie-parser');
+
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({
+    url: URI
+});
+
 fccTesting(app); //For FCC testing purposes
 app.set("view engine", "pug");
 app.use("/public", express.static(process.cwd() + "/public"));
@@ -32,12 +41,24 @@ app.use(
         cookie: {
             secure: false
         },
+        key: 'express.sid',
+        store: store
     })
 );
 
 app.use(passport.initialize());
 app.use(passport.session());
 
+
+io.use(
+    passportSocketIo.authorize({
+        cookieParser: cookieParser,
+        key: 'express.sid',
+        secret: process.env.SESSION_SECRET,
+        store: store,
+        success: onAuthorizeSuccess,
+        fail: onAuthorizeFail
+    }));
 
 myDB(async (client) => {
     const myDataBase = await client.db("database").collection("users");
@@ -52,9 +73,11 @@ myDB(async (client) => {
     });
     let currentUsers = 0;
     io.on('connection', socket => {
+        console.log('user ' + socket.request.user.name + 'connected');
         ++currentUsers;
         io.emit('user count', currentUsers);
         console.log('A user has connected');
+
 
         socket.on('disconnect', () => {
             console.log('A user has disconnected');
@@ -77,3 +100,15 @@ const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
     console.log("Listening on port " + PORT);
 });
+
+const onAuthorizeSuccess = (data, accept) => {
+    console.log('successful connection to socket.io');
+
+    accept(null, true);
+};
+
+const onAuthorizeFail = (data, message, error, accept) => {
+    if(error) throw new Error(message);
+    console.log('failed connection to socket.io: ', message);
+    accept(null, false);
+};
